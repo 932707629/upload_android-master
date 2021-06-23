7c1b624bfc23968d0a041709afd3ea262482d52c
SouShin <932707629@qq.com>
2021-06-23 14:59:10 +0800

    七牛云sdk测试

---
+++ app/src/main/java/com/qiniu/jemy/upload/utils/Config.java
@@ -0,0 +1,9 @@
+package com.qiniu.jemy.upload.utils;
+
+import android.os.Environment;
+
+public class Config {
+    public static final String SDCARD_DIR = Environment.getExternalStorageDirectory().getAbsolutePath();
+    public static final String UPTOKEN_Z0 ="dF7ZoGHY8Dc9vsw1tdX03Tj8GgA6RAhzgPqMihyr:qrwsmJ7RPh3KjLe3OzgCqAcrnsA=:eyJzY29wZSI6InF5eGgtZHVhbnNoaXBpbiIsImRlYWRsaW5lIjoxNjIzMjIwNzg4fQ==";
+//    public static final String UPTOKEN_Z0 = "bjtWBQXrcxgo7HWwlC_bgHg81j352_GhgBGZPeOW:fguUPisu0aG2J4mzvaXwNxYD5ig=:eyJzY29wZSI6InNkay16MSIsImRlYWRsaW5lIjoxNTkzNDk5NTQ5fQ==";
+}
